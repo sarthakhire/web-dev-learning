9210952734bfce5a02dc074e3162a90a9743efd1
Sarthak Hire <hiresarthak007@gmail.com>
2026-09-05 15:48:45 +0530

    learn: add basic JavaScript learning

---
+++ js/day 2/script.js
@@ -0,0 +1,32 @@
+// print hello world 
+console.log("Hello world!")
+
+
+//Let variable -->
+// defines and assign a variable 
+
+let a = "Rohit" 
+let b = 90
+
+//variable prints the assigned value
+console.log(a)
+console.log(b)
+
+//reassigning a variable
+a = "Gurunnath"
+b = "Sharma"
+
+//variable prints the reassigned value
+console.log(a)
+console.log(b)
+
+//const variable -->
+
+//assigning constant value
+const c = 56;
+console.log(c)
+
+//Can't ressign different value to const variable
+c = 10
+console.log(c)
+
